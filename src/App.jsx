a846f34650efb6cffb86e884c672fe216f7966bc
Matheus Leão <mleaopita@outlook.com>
2024-04-24 18:45:46 -0300

    criando categorias

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from './componentes/Header/Header.jsx';
 import Explicacao from './componentes/Explicacao/Explicacao.jsx';
+import Categoria from './componentes/Categoria/Categoria.jsx';
 
 function App() {
   return (
     <>
       <Header/>
       <Explicacao/>
+      <Categoria/>
     </>
   )
 }
---
+++ src/componentes/Categoria/Categoria.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from "./Categoria.module.css";
+import imagem from "./images/categorias.png";
+
+const Categoria = () => {
+    return(
+        <>
+            <div className={styles.titulo}>
+                <h2>CONFIRA EM QUE CATEGORIA SE ENCAIXA</h2>
+            </div>
+            <div className={styles.imagem}>
+                <img src={imagem} alt="todas as categorias" />
+            </div>
+        </>
+    )
+}
+
+export default Categoria;
